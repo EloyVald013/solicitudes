4845aba68944b13f8cf24671f23fc419ef250ffd
Omar Luna Hernández <42101656@uaz.edu.mx>
2025-11-12 14:13:52 -0600

    feat: Se agrega la url para atender una solicitud

--- app/solicitudes/atender_solicitudes/urls.py
+++ app/solicitudes/atender_solicitudes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import marcar_solicitud_en_proceso, cerrar_solicitud
+from .views import atender_solicitud, marcar_solicitud_en_proceso, cerrar_solicitud
 
 urlpatterns = [
+    path('atender_solicitud/<int:solicitud_id>/', atender_solicitud, name='atender_solicitud'),
     path('marcar-en-proceso/<int:solicitud_id>/', marcar_solicitud_en_proceso, name='marcar_solicitud_en_proceso'),
     path('cerrar/<int:solicitud_id>/', cerrar_solicitud, name='cerrar_solicitud'),
 ]
